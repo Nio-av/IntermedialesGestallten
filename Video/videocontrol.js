77d71b454e49026f6a144af131f7a934ad53e366
Nico <nicolas@Nios-MacBook.local>
2017-01-21 16:58:16 +0100

    Videocontroll

---
+++ Video/videocontrol.js
@@ -0,0 +1,27 @@
+function vidplay() {
+    for(var i = 0; i <= 1; i++){
+        
+           var video = document.getElementsByTagName("video")[i];
+           var button = document.getElementById("play");
+           if (video.paused) {
+              video.play();
+              button.textContent = "||";
+           } else {
+              video.pause();
+              button.textContent = ">";
+           }
+        
+        } 
+        
+    }
+
+
+function restart() {
+    for(var i = 0; i <= 1; i++){
+
+        var video = document.getElementsByTagName("video")[i];
+        video.currentTime = 0;
+    }
+}
+    
+    
